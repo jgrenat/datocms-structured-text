ba2eb6a06c2236d7935eb4728926671ae8b079bd
jgrenat <jgrenat@users.noreply.github.com>
2021-09-24 17:56:32 +0200

    End of part 3

--- src/StructuredText/Html.elm
+++ src/StructuredText/Html.elm
@@ -1,11 +1,110 @@
 module StructuredText.Html exposing (render)
 
-import Html exposing (Html, text)
-import StructuredText.Types exposing (StructuredText)
+import Html exposing (Html, code, em, hr, mark, p, pre, span, strong, text)
+import Html.Attributes exposing (class, style)
+import StructuredText.Types exposing (BlockNode(..), CodeNode(..), Mark(..), ParagraphChildNode(..), ParagraphNode(..), RootChildNode(..), SpanNode(..), StructuredText(..), ThematicBreakNode(..))
+
+
+type alias RenderParameters a msg =
+    { renderBlock : a -> Html msg
+    }
 
 
 {-| Render a StructuredText document as elm/html nodes
 -}
-render : StructuredText a -> List (Html msg)
-render structuredText =
-    Debug.todo "To do"
+render : RenderParameters a msg -> StructuredText a -> List (Html msg)
+render renderParameters (StructuredText rootChildrenNodes) =
+    List.map (renderRootChildNode renderParameters) rootChildrenNodes
+
+
+renderRootChildNode : RenderParameters a msg -> RootChildNode a -> Html msg
+renderRootChildNode renderParameters rootChildNode =
+    case rootChildNode of
+        RootThematicBreak ThematicBreakNode ->
+            renderThematicBreak
+
+        RootCode codeNode ->
+            renderCodeNode codeNode
+
+        RootParagraph paragraphNode ->
+            renderParagraphNode paragraphNode
+
+        RootBlock blockNode ->
+            renderBlockNode renderParameters blockNode
+
+        _ ->
+            text ""
+
+
+renderThematicBreak : Html msg
+renderThematicBreak =
+    hr [] []
+
+
+renderCodeNode : CodeNode -> Html msg
+renderCodeNode (CodeNode codeNodeData) =
+    let
+        attributes =
+            case codeNodeData.language of
+                Just language ->
+                    [ class ("lang-" ++ language) ]
+
+                Nothing ->
+                    []
+    in
+    pre []
+        [ code attributes [ text codeNodeData.code ]
+        ]
+
+
+renderParagraphNode : ParagraphNode a -> Html msg
+renderParagraphNode (ParagraphNode children) =
+    List.map renderParagraphChildNode children
+        |> p []
+
+
+renderParagraphChildNode : ParagraphChildNode a -> Html msg
+renderParagraphChildNode paragraphChildNode =
+    case paragraphChildNode of
+        ParagraphSpan spanNode ->
+            renderSpanNode spanNode
+
+        _ ->
+            text ""
+
+
+renderSpanNode : SpanNode -> Html msg
+renderSpanNode (SpanNode spanNodeData) =
+    case spanNodeData.marks of
+        [] ->
+            span [] [ text spanNodeData.value ]
+
+        _ ->
+            List.foldr
+                (\currentMark rendered ->
+                    case currentMark of
+                        Strong ->
+                            strong [] [ rendered ]
+
+                        Code ->
+                            code [] [ rendered ]
+
+                        Emphasis ->
+                            em [] [ rendered ]
+
+                        Underline ->
+                            span [ style "text-decoration" "underline" ] [ rendered ]
+
+                        Strikethrough ->
+                            span [ style "text-decoration" "line-through" ] [ rendered ]
+
+                        Highlight ->
+                            mark [] [ rendered ]
+                )
+                (text spanNodeData.value)
+                spanNodeData.marks
+
+
+renderBlockNode : RenderParameters a msg -> BlockNode a -> Html msg
+renderBlockNode renderParameters (BlockNode blockNodeData) =
+    renderParameters.renderBlock blockNodeData.itemContent
--- tests/HtmlTest.elm
+++ tests/HtmlTest.elm
@@ -1,9 +1,34 @@
 module HtmlTest exposing (..)
 
 import Expect exposing (Expectation)
+import Html exposing (Html, code, em, hr, img, mark, p, pre, span, strong, text)
+import Html.Attributes exposing (alt, class, src, style)
 import StructuredText.Html
-import StructuredText.Types exposing (StructuredText(..))
+import StructuredText.Types exposing (BlockNode(..), CodeNode(..), ItemId(..), Mark(..), ParagraphChildNode(..), ParagraphNode(..), RootChildNode(..), SpanNode(..), StructuredText(..), ThematicBreakNode(..))
 import Test exposing (..)
+
+
+type alias ImageItem =
+    { imageUrl : String, format : ImageFormat, alt : String }
+
+
+type ImageFormat
+    = FullWidth
+    | ImageWidth
+
+
+renderImageItem : ImageItem -> Html msg
+renderImageItem imageItem =
+    let
+        className =
+            case imageItem.format of
+                FullWidth ->
+                    "full-width"
+
+                ImageWidth ->
+                    "image-width"
+    in
+    img [ class className, src imageItem.imageUrl, alt imageItem.alt ] []
 
 
 suite : Test
@@ -17,8 +42,109 @@ suite =
                             StructuredText []
 
                         result =
-                            StructuredText.Html.render input
+                            StructuredText.Html.render { renderBlock = always (text "") } input
                     in
                     Expect.equal [] result
+            , test "should render a thematic break node" <|
+                \_ ->
+                    let
+                        input =
+                            StructuredText [ RootThematicBreak ThematicBreakNode ]
+
+                        result =
+                            StructuredText.Html.render { renderBlock = always (text "") } input
+                    in
+                    Expect.equal [ hr [] [] ] result
+            , test "should render a code node" <|
+                \_ ->
+                    let
+                        input =
+                            StructuredText
+                                [ RootCode
+                                    (CodeNode
+                                        { code = "main = 1"
+                                        , language = Just "elm"
+                                        , highlight = []
+                                        }
+                                    )
+                                ]
+
+                        result =
+                            StructuredText.Html.render { renderBlock = always (text "") } input
+                    in
+                    Expect.equal
+                        [ pre []
+                            [ code [ class "lang-elm" ] [ text "main = 1" ]
+                            ]
+                        ]
+                        result
+            , test "should render a paragraph node" <|
+                \_ ->
+                    let
+                        input =
+                            StructuredText
+                                [ RootParagraph
+                                    (ParagraphNode
+                                        [ ParagraphSpan (SpanNode { value = "Hello", marks = [] }) ]
+                                    )
+                                ]
+
+                        result =
+                            StructuredText.Html.render { renderBlock = always (text "") } input
+                    in
+                    Expect.equal
+                        [ p [] [ span [] [ text "Hello" ] ] ]
+                        result
+            , test "should render a paragraph with a span containing marks" <|
+                \_ ->
+                    let
+                        input =
+                            StructuredText
+                                [ RootParagraph
+                                    (ParagraphNode
+                                        [ ParagraphSpan (SpanNode { value = "Hello", marks = [ Emphasis, Code, Highlight ] })
+                                        , ParagraphSpan (SpanNode { value = " World!", marks = [ Underline, Strong, Strikethrough ] })
+                                        ]
+                                    )
+                                ]
+
+                        result =
+                            StructuredText.Html.render { renderBlock = always (text "") } input
+                    in
+                    Expect.equal
+                        [ p []
+                            [ em []
+                                [ code []
+                                    [ mark [] [ text "Hello" ]
+                                    ]
+                                ]
+                            , span [ style "text-decoration" "underline" ]
+                                [ strong []
+                                    [ span [ style "text-decoration" "line-through" ] [ text " World!" ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                        result
+            , test "should render a custom block" <|
+                \_ ->
+                    let
+                        input =
+                            StructuredText
+                                [ RootBlock
+                                    (BlockNode
+                                        { itemId = ItemId "58599620"
+                                        , itemContent = ImageItem "https://www.datocms-assets.com/53557/1628850590-elm-logo.svg" FullWidth "Elm logo"
+                                        }
+                                    )
+                                ]
+
+                        result =
+                            StructuredText.Html.render { renderBlock = renderImageItem } input
+                    in
+                    Expect.equal
+                        [ img [ class "full-width", src "https://www.datocms-assets.com/53557/1628850590-elm-logo.svg", alt "Elm logo" ] []
+                        ]
+                        result
             ]
         ]
